190335664e9559af43a27b8c4787bfef1a96ca40
aloqt29292 <naowo28wh@outlook.fr>
2026-09-03 14:10:07 +0600

    Update buyerbotv1.py

--- buyerbotv1.py
+++ buyerbotv1.py
@@ -260,7 +260,7 @@ def ibtn(text, **kwargs):
         kwargs.pop("icon_custom_emoji_id", None)
         kwargs.pop("style", None)
         return _InlineKeyboardButton(text=text, **kwargs)
-TOKEN = "8955454814:AAEBdCXrPWDLSNRrvy5zQkqovvkmS3CccpY"
+TOKEN = "8955454814:AAGdahaddGGHsNnEjCdnxyzOz42kRbls0jU"
 ADMIN_ID = 6995426618
 DB=os.getenv("DB_PATH","bot.db")
 FD=Path(os.getenv("FILES_DIR","uploads")); FD.mkdir(parents=True,exist_ok=True)
